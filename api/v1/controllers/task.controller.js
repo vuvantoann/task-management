01d52216ee2cec99bcb0ef1569eda285936787d5
vuvantoann <toanvu1728@gamil.com>
2025-07-04 05:15:54 +0700

    Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,9 +1,13 @@
 const Task = require('../models/task.model')
 //[GET]/api/v1/tasks
 module.exports.task = async (req, res) => {
-  const tasks = await Task.find({
+  let find = {
     deleted: false,
-  })
+  }
+  if (req.query.status) {
+    find.status = req.query.status
+  }
+  const tasks = await Task.find(find)
 
   res.json(tasks)
 }
