f322f3ce7d8d241cdebb0c5fb582394ca720ba48
vuvantoann <toanvu1728@gamil.com>
2025-07-04 05:35:44 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -4,10 +4,19 @@ module.exports.task = async (req, res) => {
   let find = {
     deleted: false,
   }
+
+  //Bộ lọc theo trạng thái
   if (req.query.status) {
     find.status = req.query.status
   }
-  const tasks = await Task.find(find)
+  //Sắp xếp theo tiêu chí
+  let sort = {}
+
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue
+  }
+  console.log(req.query)
+  const tasks = await Task.find(find).sort(sort)
 
   res.json(tasks)
 }
