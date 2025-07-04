914532ce586ec0567bcc86be9299bcb735407441
vuvantoann <toanvu1728@gamil.com>
2025-07-04 07:03:08 +0700

    Phân trang

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,4 +1,6 @@
 const Task = require('../models/task.model')
+
+const paginationHelper = require('../../../helper/pagination')
 //[GET]/api/v1/tasks
 module.exports.task = async (req, res) => {
   let find = {
@@ -9,14 +11,29 @@ module.exports.task = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status
   }
+
+  // phân trang
+  const countTasks = await Task.countDocuments(find)
+  let objectPagination = paginationHelper(
+    {
+      limitItem: 4,
+      currentPage: 1,
+    },
+    req.query,
+    countTasks
+  )
+  console.log(objectPagination)
   //Sắp xếp theo tiêu chí
   let sort = {}
 
   if (req.query.sortKey && req.query.sortValue) {
     sort[req.query.sortKey] = req.query.sortValue
   }
-  console.log(req.query)
-  const tasks = await Task.find(find).sort(sort)
+
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip)
 
   res.json(tasks)
 }
---
+++ helper/pagination.js
@@ -0,0 +1,17 @@
+module.exports = (objectPagination, query, countRecords) => {
+  if (query.page) {
+    objectPagination.currentPage = parseInt(query.page)
+  }
+
+  if (query.limit) {
+    objectPagination.limitItem = parseInt(query.limit)
+  }
+
+  objectPagination.skip =
+    (objectPagination.currentPage - 1) * objectPagination.limitItem
+
+  const totalPage = Math.ceil(countRecords / objectPagination.limitItem)
+
+  objectPagination.totalPage = totalPage
+  return objectPagination
+}
