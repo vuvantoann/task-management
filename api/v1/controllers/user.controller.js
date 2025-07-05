722f48164d50f65a5ef0c4d52dc28b01bf6667f9
vuvantoann <toanvu1728@gamil.com>
2025-07-05 15:35:27 +0700

    Tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -26,3 +26,45 @@ module.exports.register = async (req, res) => {
     })
   }
 }
+
+//[POST]/api/v1/user/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email
+  const password = md5(req.body.password)
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  })
+  if (!user) {
+    res.json({
+      code: 400,
+      message: 'Email không tồn tại',
+    })
+    return
+  }
+
+  if (password != user.password) {
+    res.json({
+      code: 400,
+      message: 'Sai mật khẩu',
+    })
+    return
+  }
+
+  if (user.status === 'inactive') {
+    res.json({
+      code: 400,
+      message: 'Tài khoản đã bị khóa',
+    })
+    return
+  }
+
+  const token = user.token
+  res.cookie('token', token)
+
+  res.json({
+    code: 200,
+    message: 'Đăng nhập thành công',
+    token: token,
+  })
+}
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -5,4 +5,6 @@ const controller = require('../controllers/user.controller')
 
 router.post('/register', controller.register)
 
+router.post('/login', controller.login)
+
 module.exports = router
