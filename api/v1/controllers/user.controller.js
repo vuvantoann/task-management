b034cd8e84bf89e61938e6a95e959665365db69c
vuvantoann <toanvu1728@gamil.com>
2025-07-10 09:48:59 +0700

    Trang thông tin cá nhân

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -177,3 +177,18 @@ module.exports.resetPassword = async (req, res) => {
     message: 'Đổi mật khẩu thành công',
   })
 }
+
+//[get]/api/v1/user/detail
+module.exports.detail = async (req, res) => {
+  const token = req.cookies.token
+  const infoUser = await User.findOne({
+    token: token,
+    deleted: false,
+  }).select('-password -token')
+
+  res.json({
+    code: 200,
+    message: 'thành công',
+    infoUser: infoUser,
+  })
+}
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -13,4 +13,6 @@ router.post('/password/otp', controller.otpPassword)
 
 router.post('/password/reset', controller.resetPassword)
 
+router.get('/detail', controller.detail)
+
 module.exports = router
